cf00fd482779ff86dc41b90679e531748783e5e8
Brian <Brian>
2016-11-30 23:51:07 -0500

    reordered parameters of the Book constructor; added abstract methods to LibraryBook.java; added ReferenceBook class

--- 01Library/LibraryBook.java
+++ 01Library/LibraryBook.java
@@ -1,3 +1,8 @@
-abstract class LibraryBook extends Book implements Comparable{
+public abstract class LibraryBook extends Book implements Comparable{
     private String callNum;
+    
+    abstract void checkout(String patron, String due);
+    abstract void returned();
+    abstract String circulationStatus();
+    abstract int compareTo(int otherCallNumber);
 }
