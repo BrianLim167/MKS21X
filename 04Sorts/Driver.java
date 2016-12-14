f92d67dd7749ac6ba01788a564eddea330f385b5
BrianLim167 <blim167@gmail.com>
2016-12-13 23:38:49 -0500

    began writing tests in Driver.java

--- 04Sorts/Driver.java
+++ 04Sorts/Driver.java
@@ -1,5 +1,11 @@
+import java.util.Arrays;
 public class Driver{
     public static void main(String[]args){
-	int[]a = new int[10];
+	int[]a = {0,8,5,7,4,6,3,2,9,1};
+	int[]b = {0,8,5,7,4,6,3,2,9,1};
+	System.out.println(Arrays.toString(a));
+	System.out.println(Arrays.toString(a.selectionSort()));
+	System.out.println(Arrays.toString(b.insertionSort()));
+	
     }
 }
