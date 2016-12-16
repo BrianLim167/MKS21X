dbd85da90aef8048375fa33c7a5ae3d26dc9e2ec
BrianLim167 <blim167@gmail.com>
2016-12-16 00:54:57 -0500

    fixed selectionSort() ; rewrote insertionSort() ; wrote bubbleSort() ; wrote tests

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -1,25 +1,10 @@
+import java.util.Arrays;
 public class Sorts{
-  /**EDIT THIS METHOD TO MATCH YOUR NAME 
-  */
-  public static String name(){
-    return "10.Lim.Brian"; 
-  }
-
-  /**Selection sort of an int array. 
-  *Upon completion, the elements of the array will be in increasing order.
-  *@param data  the elements to be sorted.
-  */
-  public static void selectionSort(int[] data){
-      for (int i=0 ; i<data.length ; i++){
-	  int low = i;
-	  for (int o=i ; o<data.length ; o++){
-	      if (data[o] < low) {
-		  low = o;
-	      }
-	  }
-	  swap(data,i,low);
-      }
-  }
+    /**EDIT THIS METHOD TO MATCH YOUR NAME 
+     */
+    public static String name(){
+	return "10.Lim.Brian"; 
+    }
 
     private static void swap(int[]data,int fI,int sI){
 	int temp = data[fI];
@@ -27,15 +12,45 @@ public class Sorts{
 	data[sI] = temp;
     }
 
-    public static void insertionSort(int[]data){
-	for (int ind=1 ; ind<data.length ; ind++){
-	    int num = data[ind];
-	    int sind = ind;
-	    while (data[sind]<data[sind-1]){
-		data[sind] = data[sind-1];
-		sind--;
+    /**Selection sort of an int array. 
+     *Upon completion, the elements of the array will be in increasing order.
+     *@param data  the elements to be sorted.
+     */
+    public static void selectionSort(int[] data){
+	for (int ind=0 ; ind<data.length ; ind++){
+	    int low = ind;
+	    for (int o=ind ; o<data.length ; o++){
+		if (data[o] < data[low]) {
+		    low = o;
+		}
 	    }
-	    data[sind] = num;
+	    swap(data,ind,low);
+	}
+    }
+
+    public static void insertionSort(int[]data){
+	for (int indSort=1 ; indSort<data.length ; indSort++){
+	    int num = data[indSort];
+	    int ind;
+	    for (ind=indSort ; ind>0 && num<data[ind-1] ; ind--){
+		data[ind]=data[ind-1];
+	    }
+	    data[ind] = num;
+	}
+    }
+
+    public static void bubbleSort(int[]data){
+        for (int indSorted=data.length ; indSorted>=0 ; indSorted--){
+	    boolean swapped=false;
+	    for (int indSwap=0 ; indSwap<indSorted-1 ; indSwap++){
+		if (data[indSwap]>data[indSwap+1]){
+		    swap(data,indSwap,indSwap+1);
+		    swapped=true;
+		}
+	    }
+	    if (!swapped){
+		return;
+	    }
 	}
     }
 }
