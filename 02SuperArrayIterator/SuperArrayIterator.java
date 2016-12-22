8a5e8b59e1dd8ecd49bd4306fa4a384c8a8ac200
BrianLim167 <blim167@gmail.com>
2016-12-22 15:16:53 -0500

    resolved changes that weren't properly updated

--- 02SuperArrayIterator/SuperArrayIterator.java
+++ 02SuperArrayIterator/SuperArrayIterator.java
@@ -3,4 +3,18 @@ public class SuperArrayIterator implements Iterator<String>{
 
     private SuperArray list;
     private int element;
+
+    public SuperArrayIterator(SuperArray list){
+	this.list = list;
+    }
+
+    public boolean hasNext(){
+	return element < list.size();
+    }
+    public String next(){
+	if (hasNext()){
+	    return list.get(element);
+	    element++;
+        }
+    }
 }
